a25423ecac83c05b16a0bfd7c481ff4b2d7cb4a4
Alisson Kendi Kohatsu <kohatsukendi@gmail.com>
2021-05-06 18:52:21 -0300

    Desafio implementado

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -14,17 +14,23 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
+      .createQueryBuilder("games")
+      .having("games.title LIKE %:title%", { title: param })
+      .getMany();
       // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this
+      .repository
+      .query("SELECT COUNT(games.id) from games"); // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
-      .createQueryBuilder()
+      .createQueryBuilder("games")
+      .where("games.id = :id", { id })
+      .getRawMany<User>();
       // Complete usando query builder
   }
 }
--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -15,16 +15,26 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const user = await 
+      this.repository.findOneOrFail({ where: { id: user_id } });
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this
+      .repository
+      .query("SELDCT * FROM users ORDER BY name ASC"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this
+      .repository
+      .query(
+        "SELECT * FROM users WHERE first_name like lower(?) AND last_name like lower(?)",
+        [`%${first_name}%`, `%${last_name}%`]
+      ); // Complete usando raw query
   }
 }
